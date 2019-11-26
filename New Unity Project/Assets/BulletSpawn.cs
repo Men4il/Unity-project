8230f79a36857805e328bbc4eafe2c894a413738
Men4il <men4ilchannel@gmail.com>
2019-11-26 09:21:19 +0200

    Empty PRJ

---
+++ New Unity Project/Assets/BulletSpawn.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawn : MonoBehaviour {
+    public GameObject prefab;
+    void Update()
+    {
+        Vector2 Position = new Vector2(Random.Range(-100f, 100f), Random.Range(10f, 35f));
+        GameObject myObj = Instantiate(prefab, Position, Quaternion.identity);
+        Destroy(myObj, 3);
+    }
+}
---
+++ New Unity Project/Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    public float speed;
+    public Rigidbody rig;
+    Vector3 movement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void Update()
+    {
+
+    }
+	// Update is called once per frame
+	void FixedUpdate () {
+       movement.x = Input.GetAxisRaw("Horizontal");
+       movement.y = Input.GetAxisRaw("Vertical");
+       rig.MovePosition(rig.position + movement * speed * Time.fixedDeltaTime);
+	}
+}
