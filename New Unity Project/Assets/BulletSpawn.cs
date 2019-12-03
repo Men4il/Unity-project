f71d163bbf4a92ea1beaf9df729923e78dbeeccb
AkSelll <44325685+AkSelll@users.noreply.github.com>
2019-12-03 08:24:51 +0200

    Added stone spawn
    Stone and Boobs

--- New Unity Project/Assets/BulletSpawn.cs
+++ New Unity Project/Assets/BulletSpawn.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class BulletSpawn : MonoBehaviour {
     public GameObject prefab;
+    public GameObject Stones;
+    int StoneCount = 0;
+    void Start()
+    {
+        StoneCount = Random.Range(0,100);
+    }
     void Update()
     {
         Vector2 Position = new Vector2(Random.Range(-100f, 100f), Random.Range(10f, 35f));
         GameObject myObj = Instantiate(prefab, Position, Quaternion.identity);
         Destroy(myObj, 3);
+        if (StoneCount >= 66)
+        {
+            GameObject stone = Instantiate(Stones, Position, Quaternion.identity);
+        }
+            
     }
 }
